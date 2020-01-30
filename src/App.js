d8df539ed67aa1d1116236a3c620bf0b3a586bd3
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-01-30 18:45:56 +0530

    Draw functionality and Disable clicks

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ class App extends Component{
 
   constructor(){
     super();
+
     this.state={
       turn: 'X',
       matrix: Array(9).fill(''),
       totalMoves: 0,
-      gameEnded: false
+      gameEnded: false,
     }
   }
    
 
   clicked(event){
+    if(this.state.gameEnded)return; //for disabling moves after a result is achieved
+    
+    //storing click events as player moves
     if(this.state.matrix[event.target.dataset.square] === ''){
       this.state.matrix[event.target.dataset.square] = this.state.turn;
       event.target.innerText = this.state.turn;
-      this.setState({
-        turn: this.state.turn==='X'?'O':'X',
-        matrix: this.state.matrix,
-        totalMoves: this.state.totalMoves++,
-      })
+      this.state.turn = this.state.turn==='X'?'O':'X';
+      this.state.matrix=this.state.matrix;
+      this.state.totalMoves++;
     }
-    var result = this.checkWinner();
+    var result = this.checkWinner(); //storing the winner result
     if(result === 'X'){
       this.setState({
         winner: 'WINNER PLAYER "X"',
@@ -33,35 +35,36 @@ class App extends Component{
       this.setState({
         winner: 'WINNER PLAYER "O"',
         gameEnded: true,
-  
       });
-    } else if(result === 'draw'){
+    } else if(result==='N'){
       this.setState({
         winner: 'Well played both it is a draw',
         gameEnded: true,
-  
       });
     }
-
-    }
-
+    
+  }
     checkWinner(){
       var winmoves = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
       var board = this.state.matrix;
+      var tot = this.state.totalMoves;
+      //WINNING CONDITION
       for(let i=0;i<winmoves.length;i++){
-        if((board[winmoves[i][0]]!=null) && board[winmoves[i][0]] === board[winmoves[i][1]] && board[winmoves[i][1]] === board[winmoves[i][2]]){
+        if(board[winmoves[i][0]] === board[winmoves[i][1]] && board[winmoves[i][1]] === board[winmoves[i][2]]){
           return board[winmoves[i][0]];
         }
-      }
-      if(this.state.totalMoves === 9){
-        return 'draw';
+        //DRAW CONDITION
+        if(tot === 9){
+          return 'N';
+        }  
       }
       
     }
+
   render(){
-  
+
   return (
-    <div id="game">
+      <div id="game">
           <h1>LET'S TIC TAC TOE</h1>
           <h3>Player 1:X Player 2:O</h3>
       <div id="board" align="center" onClick={(e)=>this.clicked(e)}>
