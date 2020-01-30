b840bf55f99b6a859cd5a2bed68150fc4198a03a
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-01-30 16:34:56 +0530

    Updates

---
+++ precache-manifest.dd936ed4552f5364ff0e13ac837846aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27bfec18e708b68aee172654203404e",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "5abe3511f8faa729260e",
+    "url": "/myapp/static/css/main.33b85e09.chunk.css"
+  },
+  {
+    "revision": "53c556e8414a93050f36",
+    "url": "/myapp/static/js/2.52879d1e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.52879d1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5abe3511f8faa729260e",
+    "url": "/myapp/static/js/main.3842f9f7.chunk.js"
+  },
+  {
+    "revision": "edb82abcda217baf13b1",
+    "url": "/myapp/static/js/runtime-main.7e519dc1.js"
+  }
+]);
